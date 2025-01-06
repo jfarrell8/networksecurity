b17958a474fbde0ff147da38c3d4135fde74f2d1
John Farrell <johndfarrell@gmail.com>
2025-01-05 22:46:02 -0600

    model training implemented

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -55,42 +55,48 @@ class DataTransformation:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
-    def initate_data_transformation(self) -> DataTransformationArtifact:
-        logging.info("Entered initation_data_transformation method of DataTransformation class")
+    def initiate_data_transformation(self)->DataTransformationArtifact:
+        logging.info("Entered initiate_data_transformation method of DataTransformation class")
         try:
             logging.info("Starting data transformation")
-            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
-            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+            train_df=DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df=DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
 
-            # training dataframe
-            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
+            ## training dataframe
+            input_feature_train_df=train_df.drop(columns=[TARGET_COLUMN],axis=1)
             target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
 
-            # testing dataframe
+            #testing dataframe
             input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
             target_feature_test_df = test_df[TARGET_COLUMN]
             target_feature_test_df = target_feature_test_df.replace(-1, 0)
 
-            preprocessor = self.get_data_transformer_object()
+            preprocessor=self.get_data_transformer_object()
+
             preprocessor_object=preprocessor.fit(input_feature_train_df)
-            transformed_input_train_feature = preprocessor_object.transform(input_feature_train_df)
-            transformed_input_test_feature = preprocessor_object.transform(input_feature_test_df)
+            transformed_input_train_feature=preprocessor_object.transform(input_feature_train_df)
+            transformed_input_test_feature =preprocessor_object.transform(input_feature_test_df)
+             
 
-            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df)]
-            test_arr = np.c_[transformed_input_test_feature, np.array(target_feature_test_df)]
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df) ]
+            test_arr = np.c_[ transformed_input_test_feature, np.array(target_feature_test_df) ]
 
-            # save numpy array data
-            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
-            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
-            save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_object)
+            #save numpy array data
+            save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
+            save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
+            save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
 
-            # prepare artifacts
-            data_transformation_artifact = DataTransformationArtifact(
+            save_object( "final_model/preprocessor.pkl", preprocessor_object,)
+
+
+            #preparing artifacts
+
+            data_transformation_artifact=DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
                 transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
                 transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
             )
-
             return data_transformation_artifact
 
         except Exception as e:
